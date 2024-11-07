0d8803920ca6e8abdb4a069afecfc31c1872b313
Bee Johnson <bjohnson@frontsteps.com>
2024-11-07 11:56:34 -0700

    extension issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
@@ -8,6 +8,22 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  reloadCount: number = 0;
+
   constructor() {}
+
+  ngOnInit(): void {
+    const savedReloadCount = localStorage.getItem('reloadCount');
+    if (savedReloadCount) {
+      this.reloadCount = parseInt(savedReloadCount);
+    }
+    this.reloadCount++;
+    localStorage.setItem('reloadCount', this.reloadCount.toString());
+  }
+
+  reset() {
+    this.reloadCount = 0;
+    localStorage.setItem('reloadCount', this.reloadCount.toString());
+  }
 }
